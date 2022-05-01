a225f662077180f43145a02f5f33fa247220541a
choewy <choewy32@gmail.com>
2022-05-01 22:41:53 +0900

    최원영 edit `dotenv` 말고 `config` 사용

--- src/config/kakao.config.ts
+++
@@ -1,6 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('app', () => ({
-  kakaoRestApiKey: process.env.KAKAO_REST_API_KEY,
-  kakaoRedirectUri: 'http://localhost:3000/auth/kakao/oauth',
-}));
