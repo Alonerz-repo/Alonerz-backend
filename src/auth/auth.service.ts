e35e568910d4f44f701a2bae80e82aef2dcad7f5
choewy <choewy32@gmail.com>
2022-05-03 21:01:06 +0900

    최원영 edit : auth에서는 로그인/회원가입만 처리할 수 있도록 역핣 분할

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,13 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserRepository } from '../user/user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
-import { AuthException } from './auth.exception';
 import { User } from 'src/user/user.entity';
-import { JwtTokens, KakaoAccount, KakaoPayload } from 'src/common/interfaces';
-import { configs } from 'src/common/configs';
-import axios from 'axios';
-
-const { adminKey } = configs.kakao;
+import { JwtTokens, KakaoPayload } from 'src/common/interfaces';
 
 @Injectable()
 export class AuthService {
@@ -17,18 +12,6 @@ export class AuthService {
     private readonly userRepository: UserRepository,
     private jwtService: JwtService,
   ) {}
-
-  // Kakao 사용자 정보 조회 API
-  private async kakaoGetUserAPI(kakaoId: string): Promise<KakaoAccount> {
-    const host = 'https://kapi.kakao.com/v2/user/me';
-    const url = `${host}?target_id_type=user_id&target_id=${kakaoId}`;
-    const headers = {
-      Authorization: `KakaoAK ${adminKey}`,
-      'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
-    };
-    const { data } = await axios.get(url, { headers });
-    return data;
-  }
 
   // KakaoId로 서비스 계정 정보 조회
   private async serviceUser(kakaoId: string): Promise<User> {
