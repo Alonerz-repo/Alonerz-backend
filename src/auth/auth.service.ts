27130e697ff1594ccb90e346b9a7e9d70b802c8f
choewy <choewy32@gmail.com>
2022-05-03 21:04:05 +0900

    최원영 edit : gender 정보 카카오 API에 의존하도록 수정

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -20,16 +20,16 @@ export class AuthService {
   }
 
   // KakaoId로 서비스 계정 회원가입 처리
-  private async serviceSignup(kakaoId: string, gender: string): Promise<void> {
-    const user = { kakaoId, gender: gender ? gender : null };
+  private async serviceSignup(kakaoId: string): Promise<void> {
+    const user = { kakaoId };
     await this.userRepository.createKakaoUser(user);
   }
 
   // 카카오 로그인 및 회원가입
   async kakaoSignupOrLogin(kakaoPayload: KakaoPayload): Promise<JwtTokens> {
-    const { kakaoId, gender } = kakaoPayload;
+    const { kakaoId } = kakaoPayload;
     const user = this.serviceUser(kakaoId);
-    if (!user) await this.serviceSignup(kakaoId, gender);
+    if (!user) await this.serviceSignup(kakaoId);
     return {
       accessToken: this.jwtService.sign({ kakaoId }),
     };
