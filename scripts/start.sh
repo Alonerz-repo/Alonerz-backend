bbc2ff303607baa7da6587ac9669902b77d297e7
choewy <choewy32@gmail.com>
2022-05-06 00:36:47 +0900

    최원영 edit : sudo 권한으로 pm2 실행

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,5 +9,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-npm install && npm run build
-npm run start > app.out.log 2> app.err.log < /dev/null &
+sudo npm install && npm run build
+sudo npm run start > app.out.log 2> app.err.log < /dev/null &
