826760d5ca5b6729e1d2a3137f69d118205c84ff
choewy <choewy32@gmail.com>
2022-05-06 01:40:41 +0900

    최원영 edit : sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,6 +5,10 @@ sudo chmod -R 777 /home/ubuntu/nest
 sudo cp /home/ubuntu/environments/nest/* /home/ubuntu/nest/config
 cd /home/ubuntu/nest
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 sudo npm install
 sudo npm run build
 npm run start:prod
