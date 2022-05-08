df813e7b7d4321171f64c0cef0a9c397507637c9
choewy <choewy32@gmail.com>
2022-05-08 20:36:08 +0900

    최원영 feat : JWT accessToken, refreshToken 구현

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,15 +1,53 @@
 import { Request, Response } from 'express';
-import { Controller, Post } from '../common/decorator/application';
+import {
+  Controller,
+  Delete,
+  Get,
+  Middleware,
+  Post,
+} from '../common/decorator/application';
+import { AuthMiddleware } from '../middleware/auth.middleware';
 import { AuthService } from '../service/auth.service';
 
-@Controller('api/auth')
-export class AuthController {
-  private readonly authService = new AuthService();
+// [ TODO ] : 의존성 주입 필요
+const authService = new AuthService();
 
-  // 카카오 AccessToken으로 로그인, 회원가입
-  @Post('kakao')
-  public async loginOrSignupWithKakao(req: Request, res: Response) {
-    const { accessToken } = req.body;
-    return await this.authService.loginWithKakao(accessToken);
+@Controller('/api/auth')
+export class AuthController {
+  // [ Complete ] : 카카오 AccessToken으로 로그인, 회원가입
+  @Post('/kakao')
+  @Middleware(AuthMiddleware.validKakaoToken)
+  public async loginOrSignupWithKakao(_: Request, res: Response) {
+    const { kakaoId } = res.locals;
+    const { statusCode, tokens } = await authService.loginWithKakao(kakaoId);
+    return res.status(statusCode).json(tokens);
+  }
+
+  // [ Complete ] : 토큰 유효성 검사
+  @Get()
+  @Middleware(AuthMiddleware.validateServiceToken)
+  public async authorized(_: Request, res: Response) {
+    const { user } = res.locals;
+    return res.status(200).json({
+      userId: user.userId,
+      kakaoId: user.kakaoId,
+    });
+  }
+
+  // [ Complete ] : 토큰 재발급
+  @Get('/refresh')
+  @Middleware(AuthMiddleware.validateServiceTokens)
+  public async reissueToken(_: Request, res: Response) {
+    const { userToken } = res.locals;
+    const { statusCode, tokens } = await authService.reissueToken(userToken);
+    console.log(tokens);
+    return res.status(statusCode).json(tokens);
+  }
+
+  // [ Complete ] : 로그아웃
+  @Delete('/logout')
+  @Middleware(AuthMiddleware.deleteServiceTokens)
+  public async logout(_: Request, res: Response) {
+    return res.status(200).send();
   }
 }
--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,42 +1,69 @@
+import { User } from '../entity/user.entity';
 import {
   UserRepository,
   UserTokenRepository,
 } from '../repository/user.repository';
 import { JwtModule } from '../utils/jwt';
-import { KakaoAPI } from '../utils/kakao';
 
 export class AuthService {
   private readonly userRepository = UserRepository;
   private readonly userTokenRepository = UserTokenRepository;
 
-  // 카카오 AccessToken으로 사용자 정보 조회
-  private async validateKakaoToken(kakaoToken: string): Promise<string> {
-    const { id } = await KakaoAPI.validate(kakaoToken);
-    if (!id) throw Error('카카오 토큰 인증 실패');
-    return id;
-  }
-
-  // 카카오 id(kakaoId)로 회원가입 처리
-  private async signupWithKakao(kakaoId: string) {
+  // [ Complete ] : 카카오 id(kakaoId)로 회원가입 처리
+  private async signupWithKakao(kakaoId: string): Promise<User> {
     const user = this.userRepository.create({ kakaoId });
     await this.userRepository.save(user);
-    return user.userId;
+    return user;
   }
 
-  // 카카오 AccessToken으로 로그인 및 회원가입 처리
-  public async loginWithKakao(kakaoToken: string) {
-    const kakaoId = await this.validateKakaoToken(kakaoToken);
-    const user = await this.userRepository.findOneBy({ kakaoId });
-    const userId = !user ? await this.signupWithKakao(kakaoId) : user.userId;
-    const payload = { kakaoId, userId: userId };
-    const accessToken = JwtModule.createAccessToken(payload);
-    const refreshToken = JwtModule.createRefreshToken();
-    await this.userTokenRepository.save({ ...payload, refreshToken });
-    return { alonerz_access: accessToken, alonerz_refresh: refreshToken };
+  // [ Complete ] : 카카오 AccessToken으로 로그인 및 회원가입 처리
+  public async loginWithKakao(kakaoId: string): Promise<{
+    statusCode: number;
+    tokens: { accessToken: string; refreshToken: string };
+  }> {
+    let statusCode = 200;
+    let user = await this.userRepository.findOneBy({ kakaoId });
+    if (!user) {
+      statusCode = 201;
+      user = await this.signupWithKakao(kakaoId);
+    }
+
+    const userId = user.userId;
+    const payload = { kakaoId, userId };
+    const tokens = {
+      accessToken: JwtModule.createAccessToken(payload),
+      refreshToken: JwtModule.createRefreshToken(),
+    };
+
+    await this.userTokenRepository.save({
+      ...payload,
+      ...tokens,
+    });
+
+    return { statusCode, tokens };
   }
 
-  // 서비스 자체 AccessToken 유효성 검사
-  public async verifyAccessToken(accessToken: string) {
-    let payload = JwtModule.verifyAccessToken(accessToken);
+  // [ Complete ] : 서비스 토큰 재발급
+  public async reissueToken(userToken: {
+    id: number;
+    userId: number;
+    kakaoId: string;
+    refreshToken: string;
+  }): Promise<{
+    statusCode: number;
+    tokens: { accessToken: string; refreshToken: string };
+  }> {
+    const { id, userId, kakaoId } = userToken;
+    const payload = { userId, kakaoId };
+    const tokens = {
+      accessToken: JwtModule.createAccessToken(payload),
+      refreshToken: JwtModule.createRefreshToken(),
+    };
+
+    await this.userTokenRepository.update(id, {
+      ...tokens,
+    });
+
+    return { statusCode: 200, tokens };
   }
 }
