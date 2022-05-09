b6e9308e000e4f102b178a827cd1d653ee7d09c9
choewy <choewy32@gmail.com>
2022-05-09 16:44:26 +0900

    최원영 edit : Redirect to client 개발 환경으로 경로 재설정

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -2,6 +2,7 @@ import {
   Body,
   Controller,
   Get,
+  Patch,
   Post,
   Req,
   Res,
@@ -13,6 +14,7 @@ import { AuthService } from 'src/service/auth.service';
 import { KakaoGuard } from 'src/guard/kakao.guard';
 import { ApiOAuth2, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthLoginDto } from 'src/dto/auth-login.dto';
+import { JwtGuard } from 'src/guard/jwt.guard';
 
 @Controller('auth')
 @ApiTags('사용자 인증 API')
@@ -32,13 +34,15 @@ export class AuthController {
   @Get('kakao')
   async kakao(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
     const kakaoId = req.user;
-    const { clientUrl } = this.configService.get('auth');
-    const redirectUrl = `${clientUrl}?kakaoId=${kakaoId}`;
+    // const { clientUrl } = this.configService.get('auth');
+    // 개발 단계에서는 아래와 같이 설정
+    const redirectUrl = `http://${req.hostname}:3000/redirect?kakaoId=${kakaoId}`;
+    // 배포 환경에서는 S3 URL로 연결
     return res.redirect(redirectUrl);
   }
 
   @ApiOperation({
-    summary: '사용자 로그인',
+    summary: '사용자 로그인 API',
     description:
       '사용자 정보가 있으면 로그인, 없으면 회원가입으로 처리한 후 AccessToken과 RefreshToken을 보내준다.',
   })
@@ -47,5 +51,27 @@ export class AuthController {
     const { kakaoId } = body;
     const result = await this.authService.loginOrSignup(kakaoId);
     return result;
+  }
+
+  @ApiOperation({
+    summary: '사용자 토큰 인증 API',
+    description:
+      'AccessToken의 유효성을 검사합니다. 만약, 토큰이 만료되었다면 새로운 토큰 발급을 위한 응답을 보냅니다.',
+  })
+  @UseGuards(JwtGuard)
+  @Get()
+  async auth(@Req() req: Request) {
+    return req.user;
+  }
+
+  @ApiOperation({
+    summary: '토큰 재발급 API',
+    description:
+      '기존의 AccessToken과 RefreshToken을 전달 받아 새로운 AccessToken과 RefreshToken을 발급합니다.',
+  })
+  @Patch()
+  async reissue(@Req() req: Request, @Body() token: { refreshToken: string }) {
+    const { authorization } = req.headers;
+    console.log(authorization, token);
   }
 }
