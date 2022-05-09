889c36e678053f479965d0ab328f6ed8d67f72c7
choewy <choewy32@gmail.com>
2022-05-09 21:32:42 +0900

    최원영 feat : Swagger Bearer 적용

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -8,27 +8,24 @@ import {
   Res,
   UseGuards,
 } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { Request, Response } from 'express';
 import { AuthService } from 'src/service/auth.service';
 import { KakaoGuard } from 'src/guard/kakao.guard';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthLoginDto } from 'src/dto/auth.dto';
 import { JwtGuard } from 'src/guard/jwt.guard';
 
-@Controller('auth')
 @ApiTags('사용자 인증 API')
+@Controller('auth')
 export class AuthController {
-  constructor(
-    private authService: AuthService,
-    private configService: ConfigService,
-  ) {}
+  constructor(private authService: AuthService) {}
 
   @ApiOperation({
     summary: '사용자 토큰 인증 API',
     description:
       'AccessToken의 유효성을 검사합니다. 만약, 토큰이 만료되었다면 새로운 토큰 발급을 위한 응답을 보냅니다.',
   })
+  @ApiBearerAuth('AccessToken')
   @UseGuards(JwtGuard)
   @Get()
   async auth(@Req() req: Request) {
@@ -44,9 +41,9 @@ export class AuthController {
   @Get('kakao')
   async kakao(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
     const kakaoId = req.user;
-    // 개발 단계에서는 아래와 같이 설정
-    // 배포 환경에서는 S3 URL로 연결
-    const redirectUrl = `http://${req.connection.remoteAddress}:3000/redirect?kakaoId=${kakaoId}`;
+    // 개발 단계에서는 아래와 같이 설정(배포 환경에서는 S3 URL로 설정)
+    const clientUrl = `http://${req.connection.remoteAddress}:3000`;
+    const redirectUrl = `${clientUrl}/redirect?kakaoId=${kakaoId}`;
     return res.redirect(redirectUrl);
   }
 
@@ -66,6 +63,7 @@ export class AuthController {
     description:
       '기존의 AccessToken과 RefreshToken을 전달 받아 새로운 AccessToken과 RefreshToken을 발급합니다.',
   })
+  @ApiBearerAuth('AccessToken')
   @Patch('token')
   async reissue(@Req() req: Request, @Body() token: { refreshToken: string }) {
     const { authorization } = req.headers;
