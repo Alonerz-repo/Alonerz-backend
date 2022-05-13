22f0b84068a38e63b234141106c3ad70c4dd8fdf
choewy <choewy32@gmail.com>
2022-05-13 20:43:56 +0900

    최원영 refactor : 모듈 단위로 폴더 구조 변경

--- src/filter/http.exception.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module, Logger } from '@nestjs/common';
-import { HttpExceptionProvider } from './http.exception.provider';
-
-@Module({
-  providers: [Logger, HttpExceptionProvider],
-})
-export class HttpExceptionModule {}
--- src/filter/http.exception.provider.ts
+++
@@ -1,7 +0,0 @@
-import { APP_FILTER } from '@nestjs/core';
-import { HttpExceptionFilter } from './http.exception.filter';
-
-export const HttpExceptionProvider = {
-  provide: APP_FILTER,
-  useClass: HttpExceptionFilter,
-};
