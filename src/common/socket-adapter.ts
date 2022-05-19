f68f750fe1acd3db898366fe51137a09d49f7345
choewy <choewy32@gmail.com>
2022-05-19 16:30:59 +0900

    최원영 feat : 소켓 연결 및 포트 공유 (5000 || 8081)

---
+++ src/common/socket-adapter.ts
@@ -0,0 +1,8 @@
+import { IoAdapter } from '@nestjs/platform-socket.io';
+
+export class SocketAdapter extends IoAdapter {
+  createIOServer(port: number, options?: any): any {
+    const server = super.createIOServer(port, options);
+    return server;
+  }
+}
