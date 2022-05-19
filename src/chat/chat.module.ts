e513a1fc46dd18e6c69d8f710f1a9a796a479dbe
KimJiWoong02 <jiwoongkim02@gmail.com>
2022-05-20 05:45:05 +0900

    김지웅 feat(ing): Socket 채팅 기본 기능 구현

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -3,9 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatRepository } from './chat.repository';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+import { ChatRoomRepository } from 'src/chatroom/chatroom.repository';
+import { ChatUserRepository } from 'src/chatuser/chatuser.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ChatRepository])],
+  imports: [
+    TypeOrmModule.forFeature([
+      ChatRepository,
+      ChatRoomRepository,
+      ChatUserRepository,
+    ]),
+  ],
   providers: [ChatService, ChatRepository],
   controllers: [ChatController],
 })
