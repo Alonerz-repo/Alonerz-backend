e4281a096fe93c74404612d77ace2501ed775f06
KimJiWoong02 <jiwoongkim02@gmail.com>
2022-05-20 05:44:12 +0900

    김지웅 feat(ing): Socket 채팅방 기본 기능 구현

--- src/chatroom/chatroom.repository.ts
+++ src/chatroom/chatroom.repository.ts
@@ -1,27 +1,40 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, QueryRunner, Repository } from 'typeorm';
 import { ChatRoom } from './chatroom.entity';
 
 @EntityRepository(ChatRoom)
 export class ChatRoomRepository extends Repository<ChatRoom> {
-  // 상대방과의 채팅방이 있는지 확인
-  // 레거시 코드이므로 상황에 맞게 수정하세요.
-  async findChatRoomWithOther(otherId: string) {
-    return await this.createQueryBuilder('chatrooms')
-      .select(['chatrooms.roomId'])
-      .leftJoinAndSelect('chatrooms.users', 'users')
-      .leftJoin('users.user', 'user')
-      .where('user.userId = :otherId', { otherId })
-      .getOne();
+  // 채팅방 존재 여부 확인 (RoomId)
+  async findOneByRoomId(roomId: string) {
+    return await this.findOne({ roomId });
   }
 
-  // 채팅방 생성
-  // 레거시 코드이므로 상황에 맞게 수정하세요.
-  async createChatRoom() {
-    return await this.save({});
+  // 채팅방 존재 여부 확인 (UserId, OtherId) - 로직 변경 필요
+  async findOneByUserId(userId: string, otherId: string) {
+    // const userRoomIds = await this.createQueryBuilder('chatrooms')
+    //   .select(['chatrooms.roomId'])
+    //   .leftJoinAndSelect('chatrooms.users', 'users')
+    //   .leftJoin('users.user', 'user')
+    //   .where('user.userId = :userId', { userId })
+    //   .getMany();
+    // const otherRoomIds = await this.createQueryBuilder('chatrooms')
+    //   .select(['chatrooms.roomId'])
+    //   .leftJoinAndSelect('chatrooms.users', 'users')
+    //   .leftJoin('users.user', 'user')
+    //   .where('user.userId = :otherId', { otherId })
+    //   .getMany();
+    // const roomId = userRoomIds.filter((it) => otherRoomIds.includes(it))[0];
+    // return roomId ? String(roomId) : undefined;
+
+    // 임시 코드 (삭제하셔도 됩니다). return roomId
+    return 'hrsnisrgnouivrnioar';
   }
 
-  // 채팅방 닫기
-  // 레거시 코드이므로 상황에 맞게 수정하세요.
+  // 채팅방 생성 트랜젝션
+  async createChatRoomTransaction(queryRunner: QueryRunner, roomId: string) {
+    await queryRunner.manager.save(ChatRoom, { roomId });
+  }
+
+  // 채팅방 닫기 (유저 2명이 다 나갔을 시)
   async deleteChatRoom(roomId: string) {
     return await this.softDelete({ roomId });
   }
