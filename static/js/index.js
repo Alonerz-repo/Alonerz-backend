bd1e03a776b6cfe1c374af3e1d5ba2a7bcdd7826
choewy <choewy32@gmail.com>
2022-05-21 03:13:35 +0900

    최원영 : CI/CD https로 재구축

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const container = document.getElementById('root');
+const datetime = document.createElement('div');
+datetime.innerHTML = `<p>time: ${new Date()}</p>`;
+container.append(datetime);
 
 navigator.geolocation.getCurrentPosition((pos) => {
   const { latitude, longitude } = pos.coords;
   const div = document.createElement('div');
   div.innerHTML = `
   <ul>
-    <li>time: ${new Date()}</li>
     <li>x : ${latitude}</li>
     <li>x : ${longitude}</li>
   </ul>
